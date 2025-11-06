dfbb0e73c8d307efc886cb3fc26126853f0f9b0f
Mario Boseski <crboseskimario@gmail.com>
2025-11-06 15:13:22 +0100

    Add home section

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,31 @@
+const Home = () => {
+    return (
+         <section class="home-section">
+        <section class="container">
+
+            <div class="home-div-class">                
+                <h1>Mario Boseski</h1>
+
+                <section class="home-section-class">
+                    <img src="projects-content/myFoto.jpg" alt="Mario-Boseski-frontend-developer"></img>
+
+                    <div class="home-info">
+                        <p>Hello i'm Mario Boseski a Frontend web-developer,
+                            transforming ideas into clean, responsive websites.
+                        </p>
+
+                        <ul class="cv-contact-links">
+                            <li><a href="Resume - Mario Boseski.pdf" class="resume-link" download="Mario Boseski - Resume.pdf">Resume</a></li>
+                            <li><a href="Pages/contact.html" class="contact-link">Contact me</a></li>
+                        </ul>                        
+                    </div>
+                </section>
+            </div>
+
+        </section>        
+    </section>
+    );
+}
+
+export default Home;
+
