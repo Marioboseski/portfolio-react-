5d1534b1eee9d90f81b78ae506798585fede5820
Mario Boseski <crboseskimario@gmail.com>
2025-11-07 13:21:48 +0100

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,24 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Projects from './pages/projects/Projects';
 import Contact from './pages/contact/Contact';
-import Navbar from './pages/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='about' element={<About />}/>
-        <Route path='projects' element={<Projects />}/>
-        <Route path='contact' element={<Contact />}/>
-      </Routes>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='about' element={<About />}/>
+          <Route path='projects' element={<Projects />}/>
+          <Route path='contact' element={<Contact />}/>
+        </Routes>
+      <Footer/>
     </div>
-  )
+  );
 }
 
 export default App
