336349b1c399c9c28c885a944609a457ce1cb6d6
Mario Boseski <crboseskimario@gmail.com>
2025-11-08 17:12:48 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,33 @@
 import "./home.css";
 
 const Home = () => {
-    return (
-         <section class="home-section">
-        <section class="container">
 
-            <div class="home-div-class">                
-                <h1>Mario Boseski</h1>
+  return (
+    <section className="home-section">
+      <section className="container">
 
-                <section class="home-section-class">
-                    <img src="projects-content/myFoto.jpg" alt="Mario-Boseski-frontend-developer"></img>
+        <div className="home-div-class">
+          <h1>Mario Boseski</h1>
 
-                    <div class="home-info">
-                        <p>Hello i'm Mario Boseski a Frontend web-developer,
-                            transforming ideas into clean, responsive websites.
-                        </p>
+          <section className="home-section-class">
+            <img src="src/assets/myFoto.jpg" alt="Mario-Boseski-frontend-developer"></img>
 
-                        <ul class="cv-contact-links">
-                            <li><a href="Resume - Mario Boseski.pdf" class="resume-link" download="Mario Boseski - Resume.pdf">Resume</a></li>
-                            <li><a href="Pages/contact.html" class="contact-link">Contact me</a></li>
-                        </ul>                        
-                    </div>
-                </section>
+            <div className="home-info">
+              <p>Hello i'm Mario Boseski a Frontend web-developer,
+                transforming ideas into clean, responsive websites.
+              </p>
+
+              <ul className="cv-contact-links">
+                <li><a href="Resume - Mario Boseski.pdf" className="resume-link" download="Mario Boseski - Resume.pdf">Resume</a></li>
+                <li><a href="Pages/contact.html" className="contact-link">Contact me</a></li>
+              </ul>
             </div>
+          </section>
+        </div>
 
-        </section>        
+      </section>
     </section>
-    );
+  );
 }
 
 export default Home;
